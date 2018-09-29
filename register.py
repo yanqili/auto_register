f95c4386e25ec1a07bffff73f34422bb498828e9
lyq <liyanqi3@jd.com>
2018-09-29 20:12:44 +0800

    增加识别工作日，存在问题：需要识别在哪个屏幕

--- register.py
+++ register.py
@@ -1,25 +1,25 @@
 
 # coding: utf-8
 
-# In[1]:
-
-
 from pymouse import PyMouse
 from pykeyboard import PyKeyboard
 import time
-m = PyMouse()
+import datetime
+from chinese_calendar import is_workday, is_holiday
 
+T = datetime.datetime.now()
+april_last = datetime.date(T.year, T.month, T.day)
 
-# In[2]:
+if(is_workday(april_last)):
+	m = PyMouse()
 
-
-m.click(220,700,1)
-time.sleep(2)
-m.click(150,80,1)
-time.sleep(3)
-m.click(600,430,1)
-time.sleep(3)
-m.click(420,140,1)
-time.sleep(3)
-m.click(300,200,1)
+	m.click(220,700,1)
+	time.sleep(2)
+	m.click(150,80,1)
+	time.sleep(3)
+	m.click(600,430,1)
+	time.sleep(3)
+	m.click(420,140,1)
+	time.sleep(3)
+	m.click(300,200,1)
 
